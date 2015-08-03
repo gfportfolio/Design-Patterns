937745ea4aa0b1097f5d9992ecc8ecfc2dee52c8
gavin-farnsworth <gavin-farnsworth@leavitt.com>
2015-08-03 13:16:52 -0600

    Started Flyweight

---
+++ gfdesignpatterns/gfdesignpatterns/flyweight/Flyweight.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gfdesignpatterns.flyweight
+{
+    public class Flyweight
+    {
+       
+
+    }
+
+    public abstract class player
+    {
+        private int _strength;
+        public int strenth { get { return _strength; } set { _strength = value; } }
+        private string _name;
+        public string name { get { return _name; } }
+        private bool _alive;
+        public bool alive { get { return _alive; } set { _alive = value; } }
+        private bool _good;
+        public bool good { get { return _good; } }
+        private string _image;
+
+
+        public player(int strenth, string name, bool good, string image)
+        {
+            _strength = strenth;
+            _name = name;
+            _alive = true;
+            _good = good;
+            _image = image;
+        }
+
+
+
+    }
+}
---
+++ gfdesignpatterns/gfdesignpatterns/flyweight/FlyweightDemo.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainMaster.Master" AutoEventWireup="true" CodeBehind="FlyweightDemo.aspx.cs" Inherits="gfdesignpatterns.flyweight.FlyweightDemo" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="scripts" runat="server">
+</asp:Content>
